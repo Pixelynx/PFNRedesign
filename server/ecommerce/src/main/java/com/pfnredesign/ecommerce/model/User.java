754472121bdfcfeafad13d429f30e2cea9612fe4
Pixelynx <tjstevens1992@gmail.com>
2025-05-13 17:24:21 -0400

    update User getter/setter implementation using @Data

--- server/ecommerce/src/main/java/com/pfnredesign/ecommerce/model/User.java
+++ server/ecommerce/src/main/java/com/pfnredesign/ecommerce/model/User.java
@@ -1,104 +1,33 @@
 package com.pfnredesign.ecommerce.model;
 
 import jakarta.persistence.*;
-import java.util.Date;
-import java.util.Objects;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "users")
+@Data
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long userId;
 
     @Column(unique = true, nullable = false)
     private String email;
 
     @Column(nullable = false)
-    private String password;
+    private String passwordHash;
 
-    @Column(name = "first_name")
     private String firstName;
-
-    @Column(name = "last_name")
     private String lastName;
+    private String phone;
 
-    @Column(name = "created_at")
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdAt;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
-    public User() {
-        this.createdAt = new Date();
-    }
-
-    public User(String email, String password, String firstName, String lastName) {
-        this.email = email;
-        this.password = password;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.createdAt = new Date();
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    // equals and hashCode methods
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(id, user.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-} 
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
--- server/ecommerce/src/main/java/com/pfnredesign/ecommerce/service/UserService.java
+++ server/ecommerce/src/main/java/com/pfnredesign/ecommerce/service/UserService.java
@@ -6,11 +6,22 @@ import com.pfnredesign.ecommerce.dto.RegistrationRequest;
 import com.pfnredesign.ecommerce.dto.UserResponse;
 import com.pfnredesign.ecommerce.model.User;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserService {
 
     UserResponse registerUser(RegistrationRequest request);
     
     AuthResponse authenticateUser(LoginRequest request);
+
+    List<User> getAllUsers();
+
+    Optional<User> getUserById(Long id);
+
+    User saveUser(User user);
+
+    void deleteUser(Long id);
     
     User getCurrentUser();
 } 
